91931ba16c35550d0c3f6c09056df21997567c2b
Dmitry Bokovikov <mitbk30@gmail.com>
2019-03-17 17:06:20 +0300

    ALGO : small fixes nth_element impl

--- Divide_and_conquer/nth_element.h
+++ Divide_and_conquer/nth_element.h
@@ -19,7 +19,7 @@ namespace divide_and_conquer_ns
 	    auto it_greater = std::partition(it_beg, it_end, [&pivot](const auto& value) { return value <= pivot; });
 	    auto it_equal = std::partition(it_beg, it_greater, [&pivot](const auto& value) { return value < pivot; });
 	    if (it_beg + i >= it_greater)
-		return nth_element_impl(it_greater, it_end, it_beg + i - it_greater);
+		return nth_element_impl(it_greater, it_end, static_cast<int>(it_beg + i - it_greater));
 	    else if (it_beg + i < it_equal)
 		return nth_element_impl(it_beg, it_equal, i);
 	    else
