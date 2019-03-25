6a350b956104d786189eef863d20f2b3b7ebb5e4
Dmitry Bokovikov <mitbk30@gmail.com>
2019-03-26 01:18:24 +0300

    ALGO : Implemented deletion from binary tree + fully covered tests

--- Data_structures/Binary_search_tree/Tests/Binary_search_tree_tests.cpp
+++ Data_structures/Binary_search_tree/Tests/Binary_search_tree_tests.cpp
@@ -54,14 +54,19 @@ namespace data_structures_ns::tests
 	TEST_METHOD(test_size)
 	{
 	    binary_search_tree<ll> tree;
-	    auto data = util_ns::gen_data(1'000);
+	    const ll data_size = 1'000;
+	    const auto data = util_ns::gen_data(data_size);
 	    Assert::AreEqual(tree.size(), 0, L"size should be zero");
-	    for (ll i : range_ns::xrange(size(data)))
+	    for (ll i : range_ns::xrange(data_size))
 	    {
 		tree.insert(data[i]);
 		Assert::AreEqual(tree.size(), static_cast<int>(i + 1), L"unexpected tree size");
 	    }
-	    // TBD : check deletion
+	    for (ll i : range_ns::xrange(data_size))
+	    {
+		tree.erase(data[i]);
+		Assert::AreEqual(tree.size(), static_cast<int>(data_size - i - 1), L"unexpected tree size");
+	    }
 	}
 	TEST_METHOD(test_get_sorted)
 	{
@@ -72,7 +77,97 @@ namespace data_structures_ns::tests
 	    auto sorted = tree.get_sorted();
 	    range_ns::sort(elements);
 	    Assert::IsTrue(sorted == elements);
-	    // TBD : check deletion
+	    std::multiset<ll> mset(cbegin(sorted), cend(sorted));
+	    util_ns::random_permutate(sorted);
+	    bool need_log = false;
+	    const auto file_out_path = fs::path("M:\\git\\Algo\\bfs_tree.txt");
+	    if (need_log) std::ofstream().open(file_out_path);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    for (ll val : sorted)
+	    {
+		tree.erase(val);
+		mset.erase(mset.find(val));
+		if (need_log) tree.test_print_in_bfs_order(file_out_path);
+		Assert::IsTrue(tree.get_sorted() == std::vector<ll>(cbegin(mset), cend(mset)));
+	    }
+	}
+
+	TEST_METHOD(test_delete_with_less_than_two_childrens)
+	{
+	    binary_search_tree<ll> tree;
+	    tree.insert(1);
+	    tree.erase(1);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{});
+	    Assert::IsTrue(tree.empty());
+	    tree.insert(1);
+	    tree.insert(2);
+	    tree.erase(1);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{2});
+	    Assert::IsTrue(tree.size() == 1);
+	    tree.insert(1);
+	    tree.erase(1);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{2});
+	    Assert::IsTrue(tree.size() == 1);
+	    tree.reset();
+	    Assert::IsTrue(tree.empty());
+	    for (ll val : {1, 2, 3, 4, 5, 6})
+		tree.insert(val);
+	    Assert::IsTrue(tree.size() == 6);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{1, 2, 3, 4, 5, 6});
+	    for (ll val : {2, 3, 4, 5})
+		tree.erase(val);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{1, 6});
+	    Assert::IsTrue(tree.size() == 2);
+	    tree.erase(1);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{6});
+	    tree.erase(6);
+	    Assert::IsTrue(tree.get_sorted() == std::vector<ll>{});
+	    for (ll val : {1, 2, 3, 4, 5, 6})
+		tree.insert(val);
+	    for (ll val : {1, 2, 3, 4, 5, 6})
+	    {
+		tree.erase(val);
+		const ll expected_size = 6 - val;
+		Assert::IsTrue(tree.size() == expected_size);
+		std::vector<ll> expected(expected_size);
+		std::iota(begin(expected), end(expected), val + 1);
+		Assert::IsTrue(tree.get_sorted() == expected);
+	    }
+
+	    for (ll val : { 6, 5, 4, 3, 2, 1})
+		tree.insert(val);
+	    for (ll val : { 6, 5, 4, 3, 2, 1})
+	    {
+		tree.erase(val);
+		const ll expected_size = val - 1;
+		Assert::IsTrue(tree.size() == expected_size);
+		std::vector<ll> expected(expected_size);
+		std::iota(begin(expected), end(expected), 1);
+		Assert::IsTrue(tree.get_sorted() == expected);
+	    }
+	}
+
+	TEST_METHOD(test_delete_with_than_two_childrens)
+	{
+	    binary_search_tree<ll> tree;
+	    for (ll val : {4, 3, 7, 8, 1, 5, 2, 6})
+		tree.insert(val);
+	    bool need_log = false;
+	    const auto file_out_path = fs::path("M:\\git\\Algo\\bfs_tree.txt");
+	    if (need_log) std::ofstream().open(file_out_path);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    tree.erase(7);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    Assert::IsTrue(tree.get_in_bfs_order() == std::vector<ll>{4, 3, 8, 1, 5, 2, 6});
+	    tree.erase(4);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    Assert::IsTrue(tree.get_in_bfs_order() == std::vector<ll>{5, 3, 8, 1, 6, 2});
+	    tree.erase(5);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    Assert::IsTrue(tree.get_in_bfs_order() == std::vector<ll>{6, 3, 8, 1, 2});
+	    tree.erase(6);
+	    if (need_log) tree.test_print_in_bfs_order(file_out_path);
+	    Assert::IsTrue(tree.get_in_bfs_order() == std::vector<ll>{8, 3, 1, 2});
 	}
     };
 }
--- Data_structures/Binary_search_tree/binary_search_tree.h
+++ Data_structures/Binary_search_tree/binary_search_tree.h
@@ -4,11 +4,29 @@
 #include <Util/ranges.h>
 #include <Util/Util.h>
 
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
 namespace data_structures_ns
 {
     template<class T>
     class binary_search_tree
     {
+    private:
+	struct node
+	{
+	    node(const T& i_value) : m_value(i_value) {}
+
+	    std::shared_ptr<node> m_left_ptr;
+	    std::shared_ptr<node> m_right_ptr;
+	    std::shared_ptr<node> m_parent_ptr;
+	    T m_value;
+	};
+
+	using shared_node_ptr = std::shared_ptr<node>;
+
     public:
 	void insert(const T& i_value)
 	{
@@ -40,24 +58,73 @@ namespace data_structures_ns
 
 	void erase(const T& i_elem)
 	{
+	    auto find_ptr_opt = find_int(i_elem);
+	    if (!find_ptr_opt)
+		return;
+	    auto find_ptr = find_ptr_opt.value();
+	    // no children
+	    if (!find_ptr->m_left_ptr && !find_ptr->m_right_ptr)
+	    {
+		if (find_ptr->m_parent_ptr)
+		{
+		    if (find_ptr->m_parent_ptr->m_left_ptr == find_ptr)
+			find_ptr->m_parent_ptr->m_left_ptr.reset();
+		    else
+			find_ptr->m_parent_ptr->m_right_ptr.reset();
+		}
+		else
+		{
+		    m_tree_ptr.reset();
+		}
+	    }
+	    else if (!find_ptr->m_left_ptr || !find_ptr->m_right_ptr)
+	    {
+		const bool has_left = find_ptr->m_left_ptr != nullptr;
+		if (find_ptr->m_parent_ptr)
+		{
+		    const bool parent_ptr_left = find_ptr->m_parent_ptr->m_left_ptr == find_ptr;
+		    if (parent_ptr_left)
+			find_ptr->m_parent_ptr->m_left_ptr = has_left ? find_ptr->m_left_ptr : find_ptr->m_right_ptr;		
+		    else
+			find_ptr->m_parent_ptr->m_right_ptr = has_left ? find_ptr->m_left_ptr : find_ptr->m_right_ptr;
+		    (has_left ? find_ptr->m_left_ptr : find_ptr->m_right_ptr)->m_parent_ptr = find_ptr->m_parent_ptr;
+		}
+		else
+		{
+		    m_tree_ptr = has_left ? find_ptr->m_left_ptr : find_ptr->m_right_ptr;
+		    (has_left ? find_ptr->m_left_ptr : find_ptr->m_right_ptr)->m_parent_ptr = nullptr;
+		}
+	    }
+	    else
+	    {
+		auto left_ptr = find_ptr->m_right_ptr->m_left_ptr;
+		if (!left_ptr)
+		{
+		    auto right_ptr_value = find_ptr->m_right_ptr->m_value;
+		    auto right_ptr = find_ptr->m_right_ptr;
+		    find_ptr->m_right_ptr = right_ptr->m_right_ptr;
+		    if (right_ptr->m_right_ptr)
+			right_ptr->m_right_ptr->m_parent_ptr = find_ptr;
+		    find_ptr->m_value = right_ptr_value;
+		}
+		else
+		{
+		    while (left_ptr->m_left_ptr)
+			left_ptr = left_ptr->m_left_ptr;
+		    auto left_ptr_value = left_ptr->m_value;
+		    find_ptr->m_value = left_ptr_value;
+		    if (left_ptr->m_right_ptr)
+			left_ptr->m_right_ptr->m_parent_ptr = left_ptr->m_parent_ptr;
+		    left_ptr->m_parent_ptr->m_left_ptr = left_ptr->m_right_ptr;
+		}
+	    }
 
+	    --m_size;
 	}
 
 	bool find(const T& i_elem) const
 	{
-	    std::function<bool(const shared_node_ptr&)> find_impl;
-	    find_impl = [&find_impl, &i_elem](const shared_node_ptr& i_node_ptr)
-	    {
-		if (!i_node_ptr)
-		    return false;
-
-		if (i_node_ptr->m_value == i_elem)
-		    return true;
-
-		return i_node_ptr->m_value > i_elem ? find_impl(i_node_ptr->m_left_ptr) : find_impl(i_node_ptr->m_right_ptr);
-	    };
-
-	    return find_impl(m_tree_ptr);
+	    return find_int(i_elem).has_value();
 	}
 
 	void reset()
@@ -84,6 +151,9 @@ namespace data_structures_ns
 	    std::function<void(const shared_node_ptr&)> visit;
 	    visit = [&visit, &res, this](const shared_node_ptr& i_tree_ptr)
 	    {
+		if (!i_tree_ptr)
+		    return;
+
 		if (i_tree_ptr->m_left_ptr)
 		    visit(i_tree_ptr->m_left_ptr);
 
@@ -97,47 +167,77 @@ namespace data_structures_ns
 	    return res;
 	}
 
-	std::vector<T> get_in_bfs_order() const
+	std::vector<T> get_in_bfs_order(std::function<void(const node&, ll)> i_func = nullptr) const
 	{
 	    std::vector<T> res;
 	    res.reserve(m_size);
 
-	    std::queue<const node*> queue;
+	    std::queue<std::pair<const node*, ll>> queue;
 	    if (m_tree_ptr)
-		queue.emplace(m_tree_ptr.get());
-	    while (!empty(queue))
+		queue.emplace(m_tree_ptr.get(), 1);
+	    while (!std::empty(queue))
 	    {
-		auto ptr = queue.front();
+		auto[ptr, depth] = queue.front();
 		queue.pop();
 		res.emplace_back(ptr->m_value);
+		if (i_func)
+		    i_func(*ptr, depth);
 		if (ptr->m_left_ptr)
-		    queue.emplace(ptr->m_left_ptr.get());
+		    queue.emplace(ptr->m_left_ptr.get(), depth + 1);
 		if (ptr->m_right_ptr)
-		    queue.emplace(ptr->m_right_ptr.get());
+		    queue.emplace(ptr->m_right_ptr.get(), depth + 1);
 	    }
 	    return res;
 	}
 
 	int size() const { return m_size; }
 
-	int height() const { return m_height; }
+	bool empty() const { return !m_size; }
+
+	void test_print_in_bfs_order(const fs::path& i_path)
+	{
+	    std::ofstream out_file_stream(i_path, std::ios::out | std::ios::app);
+	    if (!out_file_stream.is_open())
+		throw std::runtime_error("error open file " + i_path.string());
+	    ll i = 1;
+	    out_file_stream << "depth " << 1 << " : ";
+	    auto bfs_func = [&i, &out_file_stream](const node& i_node, ll depth)
+	    {
+		if (depth > i)
+		{
+		    i = depth;
+		    out_file_stream << "\ndepth " << depth << " : ";
+		}
+		out_file_stream
+		    << "value (" << i_node.m_value << ")"
+		    << " parent (" << (i_node.m_parent_ptr ? std::to_string(i_node.m_parent_ptr->m_value) : "nullptr") << ")"
+		    << " left (" << (i_node.m_left_ptr ? std::to_string(i_node.m_left_ptr->m_value) : "nullptr") << ")"
+		    << " right (" << (i_node.m_right_ptr ? std::to_string(i_node.m_right_ptr->m_value) : "nullptr") << ")"
+		    << ", ";
+	    };
+	    get_in_bfs_order(bfs_func);
+	    out_file_stream << "\n---------------------\n";
+	}
 
     private:
+	std::optional<shared_node_ptr> find_int(const T& i_elem) const
+	{
+	    std::function<std::optional<shared_node_ptr>(const shared_node_ptr&)> find_impl;
+	    find_impl = [&find_impl, &i_elem](const shared_node_ptr& i_node_ptr) -> std::optional<shared_node_ptr>
+	    {
+		if (!i_node_ptr)
+		    return std::nullopt;
 
+		if (i_node_ptr->m_value == i_elem)
+		    return std::make_optional<shared_node_ptr>(i_node_ptr);
+
+		return i_node_ptr->m_value > i_elem ? find_impl(i_node_ptr->m_left_ptr) : find_impl(i_node_ptr->m_right_ptr);
+	    };
+
+	    return find_impl(m_tree_ptr);
+	}
 
     private:	
-	struct node
-	{
-	    node(const T& i_value) : m_value(i_value) {}
-
-	    std::shared_ptr<node> m_left_ptr;
-	    std::shared_ptr<node> m_right_ptr;
-	    std::shared_ptr<node> m_parent_ptr;
-	    T m_value;
-	};
-
-	using shared_node_ptr = std::shared_ptr<node>;
-
 	shared_node_ptr m_tree_ptr;
 	int m_size = 0;
     };
